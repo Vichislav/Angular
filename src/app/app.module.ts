0547e1d1cea8703339120792d886bafcb52808f2
Svalyan <you@example.com>
2022-02-20 10:14:27 +0600

    new study angular_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
-import { ProductComponent } from './components/product/product.component';
-import { ReviewComponent } from './components/review/review.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    ProductComponent,
-    ReviewComponent
+    UserComponent
   ],
   imports: [
     BrowserModule,
