1c3ae8579a607fc5dc76e2fe446589f34f09d3b6
Svalyan <you@example.com>
2022-03-04 22:10:59 +0600

    new study angular_7

--- src/app/components/foo/foo.component.ts
+++ src/app/components/foo/foo.component.ts
@@ -1,33 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-foo',
   templateUrl: './foo.component.html',
   styleUrls: ['./foo.component.scss']
 })
-export class FooComponent implements OnInit {
+export class FooComponent {
 
   @Input()
-  counter = 0;
-
-  @Output()
-  counterChange = new EventEmitter<number>();
-  @Input() // то что принемаем в компонент
-  title!: string;
-
-  @Output() // то что подаем на выход из компонента
-  sayHi = new EventEmitter<string>();
-
-  constructor() {
-    console.log('constructor', this.title);
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit', this.title);
-  }
-
-  onSayHi() {
-    this.sayHi.emit(this.title + 'Hello');
-  }
+  template?: TemplateRef<{ $implicit: number }>;
 
 }
